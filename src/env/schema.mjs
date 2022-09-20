85cfe48df40f18d3452fffbe12584e66faf29472
Václav Piroutek <vasek.pirou@gmail.com>
2022-09-20 16:43:02 +0200

    added remove elements by query util

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -17,6 +17,7 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_GOOGLE_API_KEY: z.string(),
 });
 
 /**
@@ -27,4 +28,5 @@ export const clientSchema = z.object({
  */
 export const clientEnv = {
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
 };
